0b5df40d55ef95fc04c65aada6c9914b4f014254
Anton Todorov <shadowtabris84@gmail.com>
2020-08-29 00:49:32 +0300

    create CRUD operations with mikro-orm and graphql

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { MikroORM } from "@mikro-orm/core";
 
 import microConfig from "./mikro-orm.config";
 import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
@@ -14,9 +15,10 @@ const main = async () => {
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver],
+      resolvers: [HelloResolver, PostResolver],
       validate: false,
     }),
+    context: () => ({ em: orm.em }),
   });
 
   apolloServer.applyMiddleware({ app });
