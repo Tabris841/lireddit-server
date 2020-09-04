6d7e1d5aa27c6c558fe6d33d8ad2b35d7b8ea192
Anton Todorov <shadowtabris84@gmail.com>
2020-09-04 12:29:10 +0300

    add  Many to One relationship  between Posts and Users and add isAuth middleware

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,6 +1,27 @@
-import { Arg, ID, Mutation, Query, Resolver } from 'type-graphql';
+import {
+  Arg,
+  Ctx,
+  Field,
+  ID,
+  InputType,
+  Mutation,
+  Query,
+  Resolver,
+  UseMiddleware,
+} from 'type-graphql';
 
 import { Post } from '../entities/Post';
+import { MyContext } from '../types';
+import { isAuth } from '../middleware/isAuth';
+
+@InputType()
+class PostInput {
+  @Field()
+  title: string;
+
+  @Field()
+  text: string;
+}
 
 @Resolver()
 export class PostResolver {
@@ -15,8 +36,12 @@ export class PostResolver {
   }
 
   @Mutation(() => Post)
-  async createPost(@Arg('title') title: string): Promise<Post> {
-    return Post.create({ title }).save();
+  @UseMiddleware(isAuth)
+  async createPost(
+    @Arg('input') input: PostInput,
+    @Ctx() { req }: MyContext
+  ): Promise<Post> {
+    return Post.create({ ...input, creatorId: req.session!.userId }).save();
   }
 
   @Mutation(() => Post, { nullable: true })
